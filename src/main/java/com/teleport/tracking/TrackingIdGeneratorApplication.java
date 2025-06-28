653edafe87967628d98d21ccdb708d8b049ea74a
saila-lopa <sailacse@gmail.com>
2025-06-28 20:22:12 +0200

    init with basic setup

---
+++ src/main/java/com/teleport/tracking/TrackingIdGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.teleport.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingIdGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingIdGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teleport/tracking/TrackingIdGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teleport.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingIdGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
